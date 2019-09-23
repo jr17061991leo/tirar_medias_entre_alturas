cafef8d9a3a5467368ed10344398fd0c7696f06d
jr17061991leo <jr17061991leo@gmail.com>
2019-09-23 17:58:10 -0300

    leitor de alturas para tirar média

---
+++ src/leitorAlturas/programa.java
@@ -0,0 +1,35 @@
+/* programa para leitura de alturas e tirar a média entre elas*/
+package leitorAlturas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class programa {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Digite a quantidade de alturas a serem testadas");
+		int numAlturas = in.nextInt();
+		double soma = 0;
+		double media = 0;
+		double[] alturas = new double[numAlturas];
+
+		for (int i = 0; i < alturas.length; i++) {
+
+			System.out.println("digite a altura na posicao " + i + ": ");
+			alturas[i] = in.nextDouble();
+
+		}
+		for (int i = 0; i < alturas.length; i++) {
+			soma += alturas[i];
+
+		}
+		media = soma / alturas.length;
+		System.out.printf("a media é: %.2f%n", media);
+
+		in.close();
+	}
+
+}
